6fcba664823731f8c522be58b9faa69a104b7ca2
GLDTRA <guga_floriano13@hotmail.com>
2023-01-31 10:24:01 -0300

    teste

--- src/main/java/com/generation/blogpessoal/controller/PostagemController.java
+++ src/main/java/com/generation/blogpessoal/controller/PostagemController.java
@@ -2,12 +2,11 @@ package com.generation.blogpessoal.controller;
 
 import com.generation.blogpessoal.model.Postagem;
 import com.generation.blogpessoal.repository.PostagemRespository;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -17,19 +16,22 @@ Receberá requisições que serão compostas por:
 URL: Endereço da requisição (endpoint)
 Verbo: Define qual Método HTTP será acionado na Classe controladora.
 Corpo da requisição (Request Body):
-Objeto que contém os dados que serão persistidos no Banco de dadas. Nem toda a
-requisição enviará dados no Corpo da Requisição.
+Objeto que contém os dados que serão persistidos no Banco de dados.
 */
 @RequestMapping("/postagens")
 @CrossOrigin(origins = "*", allowedHeaders = "*") //Permite que o frontend (heroku e outros) acesse por qualquer domínio:
 //Geralmente , substitui o * pelo url do
 public class PostagemController {
-    @Autowired //
+    @Autowired
     private PostagemRespository postagemRepository;
-
 
     @GetMapping
     public ResponseEntity<List<Postagem>> getAll(){
         return ResponseEntity.ok(postagemRepository.findAll());
+    }
+
+    public ResponseEntity<Postagem> post(@Valid @RequestBody Postagem postagem){
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(postagemRepository.save(postagem));
     }
 }
